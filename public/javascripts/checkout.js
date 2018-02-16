4a54ff53cc21b914f601a5578bb1ce6400aee6e4
Vaibhav Kailas Bachhav <bvaibhav.95@gmail.com>
2018-02-16 18:20:45 +0530

    update

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -6,15 +6,60 @@ var picker = new Pikaday(
     maxDate: new Date(2020, 12, 31),
     yearRange: [2000,2020]
 });
-$(document).ready(function() {
-    $("#fixDelTime").hide();
-        $("input[name=delTime]").on("change", function() {
-
-            var test = $(this).val();
-            if (test==9){
-                $("#fixDelTime").show();
-            }else {
-                $("#fixDelTime").hide();
-            }
-        });
+ $("#fixDelTime").hide();
+$("input[name=delTime]").on("change", function() {
+    var test = $(this).val();
+    if (test==9){
+        $("#fixDelTime").show();
+    }else {
+        $("#fixDelTime").hide();
+    }
 });
+// Add more cake code...
+    var counter = 0;
+    $("#addrow").on("click", function () {
+        var newRow = $("<tr>");
+        var cols = "";
+
+        cols += '<td class="col-md-2"><input id="" class="form-control form-control-sm cake_id" type="text"></td>'+
+        '<td class="col-md-3">Exotic black forest</td>'+
+        '<td class="col-md-1">Rs.360</td>'+
+        '<td class="col-md-2">'+
+            '<div class="form-group">'+
+                '<select class="form-control form-control-sm" id="available_cake_weight">'+
+                '<option>1</option>'+
+                '<option>2</option>'+
+                '<option>3</option>'+
+                '<option>4</option>'+
+                '<option>5</option>'+
+                '</select>'+
+            '</div>'+
+        '</td>'+
+        '<td class="col-md-3">'+
+            '<input id="" class="form-control form-control-sm" type="text">'+
+        '</td>'; 
+
+        cols += '<td><input id="removeCake" type="button" class="ibtnDel btn btn-sm" value="X"></td>';
+        newRow.append(cols);
+        $("table.order-list").append(newRow);
+        counter++;
+    });
+
+    $("table.order-list").on("click", ".ibtnDel", function (event) {
+        $(this).closest("tr").remove();       
+        counter -= 1
+    });
+
+// Cake ID autocomplete
+    var cakeIDs = [
+        "BF001",
+        "BF002",
+        "BF003",
+        "BS001",
+        "BS002",
+        "BS003",
+        "CC001",
+      ];
+      $(".cake_id").autocomplete({
+        source: cakeIDs
+      });
--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/checkout', function(req, res, next) {
 router.get('/cart', function(req, res, next) {
   res.render('shoppingCart');
 });
-router.get('/order', function(req, res, next) {
+router.post('/order', function(req, res, next) {
   res.render('final');
 });
 router.get('/events', function(req, res, next) {
@@ -42,8 +42,75 @@ router.get('/products/butterscotch', function(req, res, next) {
     }
     res.render('products', {cakes: cakeChunks});
   });
-});router.get('/products/chocolate', function(req, res, next) {
+});
+router.get('/products/chocolate', function(req, res, next) {
   Cake.find({category:'ch'},function(err, docs){
+    var cakeChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+        cakeChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('products', {cakes: cakeChunks});
+  });
+});
+
+router.get('/products/fruit', function(req, res, next) {
+  Cake.find({category:'fc'},function(err, docs){
+    var cakeChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+        cakeChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('products', {cakes: cakeChunks});
+  });
+});
+
+router.get('/products/pineapple', function(req, res, next) {
+  Cake.find({category:'pa'},function(err, docs){
+    var cakeChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+        cakeChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('products', {cakes: cakeChunks});
+  });
+});
+
+router.get('/products/redvelvet', function(req, res, next) {
+  Cake.find({category:'rv'},function(err, docs){
+    var cakeChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+        cakeChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('products', {cakes: cakeChunks});
+  });
+});
+
+router.get('/products/strawberryvanilla', function(req, res, next) {
+  Cake.find({category:'sv'},function(err, docs){
+    var cakeChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+        cakeChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('products', {cakes: cakeChunks});
+  });
+});
+
+router.get('/products/truffle', function(req, res, next) {
+  Cake.find({category:'tf'},function(err, docs){
+    var cakeChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+        cakeChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('products', {cakes: cakeChunks});
+  });
+});
+
+router.get('/products/whiteforest', function(req, res, next) {
+  Cake.find({category:'wf'},function(err, docs){
     var cakeChunks = [];
     var chunkSize = 3;
     for (var i = 0; i < docs.length; i += chunkSize) {
