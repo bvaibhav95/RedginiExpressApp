4b17c4eb4201d96125807a1546cde35408763f8d
Vaibhav Kailas Bachhav <bvaibhav.95@gmail.com>
2018-08-02 22:01:48 +0530

    facebook-login-issue

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,20 +4,36 @@
 //         type: "GET",
 //         url:"/products/blackforest",
 //         success: function(data){
-            
+
 //         }
 //     });
 // });
 //==========counter function starts=========//
-$('.start_count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 7000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
+$(".start_count").each(function() {
+    $(this)
+        .prop("Counter", 0)
+        .animate(
+            {
+                Counter: $(this).text()
+            },
+            {
+                duration: 7000,
+                easing: "swing",
+                step: function(now) {
+                    $(this).text(Math.ceil(now));
+                }
+            }
+        );
 });
 //=============counter function ends============//
+//If you want to execute this line only once per browser session you can use sessionStorage. When you set a variable on sessionStorage it keeps its value until the browser closes (e.g. until you close Google Chrome).
+if (!sessionStorage.alreadyClicked) {
+    $("#refernearnModal").modal("show");
+    sessionStorage.alreadyClicked = true;
+}
+
+//If you want the line to be executed only once per page session (which means once every page refresh)
+// if (!window.alreadyClicked) {
+//     $("#refernearnModal").modal("show");
+//     alreadyClicked = true;
+// }
